06d9d7b380e2b789b7c65ccb24e538db6e71ee11
xceejay <joelkofiamoako@gmail.com>
2021-03-29 13:27:09 +0000

    added

--- views/js/material-dashboard.js
+++ views/js/material-dashboard.js
@@ -1,18 +1,5 @@
 /*!
-
- =========================================================
- * Material Dashboard - v2.1.2
- =========================================================
-
- * Product Page: https://www.creative-tim.com/product/material-dashboard
- * Copyright 2020 Creative Tim (http://www.creative-tim.com)
-
- * Designed by www.invisionapp.com Coded by www.creative-tim.com
-
- =========================================================
-
- * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
+Joel Kofi Amoako
  */
 
 (function() {
