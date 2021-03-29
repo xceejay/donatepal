08d67296582ebf3ee443d0651604a2fdba1659f4
xceejay <joelkofiamoako@gmail.com>
2021-03-29 17:31:30 +0000

    push

--- controllers/accountcontroller.go
+++ controllers/accountcontroller.go
@@ -68,7 +68,37 @@ func (accountcontroller AccountController) HandleLogout(c *gin.Context) {
 	c.Redirect(http.StatusTemporaryRedirect, "/")
 }
 
-func (accountController AccountController) HandleAccountPage(c *gin.Context) {
+// func (accountController AccountController) HandleAccountPage(c *gin.Context) {
+// 	session := sessions.Default(c)
+// 	username := session.Get(userkey)
+
+// 	usernameSessionstring := fmt.Sprintf("%s", username)
+
+// 	myUrl, err := url.Parse(c.Request.RequestURI)
+
+// 	if err != nil {
+// 		log.Errorf("ERROR: %s\nHandleAccountPage: Could Not Parse Request URI\nRedirecting to 404\n", err)
+// 		c.Redirect(http.StatusNotFound, "/404")
+// 	}
+// 	urlUsername := path.Base(myUrl.Path)
+// 	fmt.Printf("Request URI: %s\nBase: %s\n", c.Request.RequestURI, urlUsername)
+
+// 	if usernameSessionstring != urlUsername {
+
+// 		c.Redirect(http.StatusPermanentRedirect, "/login")
+// 		return
+// 	}
+
+// 	if urlUsername == usernameSessionstring {
+
+// 		location := fmt.Sprintf("/%s/%s", "account", username)
+
+// 		fmt.Println("LOCATION:", location)
+// 		accountController.ServeAccountPage(c)
+// 	}
+// }
+
+func (accountController AccountController) HandleAdminAccountPage(c *gin.Context) {
 	session := sessions.Default(c)
 	username := session.Get(userkey)
 
@@ -78,7 +108,7 @@ func (accountController AccountController) HandleAccountPage(c *gin.Context) {
 
 	if err != nil {
 		log.Errorf("ERROR: %s\nHandleAccountPage: Could Not Parse Request URI\nRedirecting to 404\n", err)
-		c.Redirect(http.StatusPermanentRedirect, "/404")
+		c.Redirect(http.StatusNotFound, "/404")
 	}
 	urlUsername := path.Base(myUrl.Path)
 	fmt.Printf("Request URI: %s\nBase: %s\n", c.Request.RequestURI, urlUsername)
@@ -94,7 +124,7 @@ func (accountController AccountController) HandleAccountPage(c *gin.Context) {
 		location := fmt.Sprintf("/%s/%s", "account", username)
 
 		fmt.Println("LOCATION:", location)
-		accountController.ServeAccountPage(c)
+		accountController.ServeAdminAccountPage(c)
 	}
 }
 
@@ -106,7 +136,7 @@ func (accountController AccountController) HandlePayment(c *gin.Context) {
 /////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////MISC////////////////////////////////////////////////
 
-// login is a handler that parses a form and checks for specific data
+// // login is a handler that parses a form and checks for specific data
 func (accountcontroller AccountController) PerformLogin(c *gin.Context) {
 	session := sessions.Default(c)
 	username := c.PostForm("username")
@@ -119,7 +149,7 @@ func (accountcontroller AccountController) PerformLogin(c *gin.Context) {
 	}
 
 	// Check for username and password match from a database
-	if username != "xceejay" || password != "1234" {
+	if username != "admin" || password != "1234" {
 		c.JSON(http.StatusUnauthorized, gin.H{"error": "Authentication failed"})
 		return
 	}
@@ -135,10 +165,46 @@ func (accountcontroller AccountController) PerformLogin(c *gin.Context) {
 
 }
 
-func (accountController AccountController) ServeAccountPage(c *gin.Context) {
+func (accountController AccountController) ServeAdminAccountPage(c *gin.Context) {
+	accountController.ServeAdminAccountOverviewPage(c)
+}
 
+func (accountController AccountController) ServeAdminAccountOverviewPage(c *gin.Context) {
+	content := accountController.getDashboardContent("overview", c)
+	c.Data(http.StatusOK, "text/html; charset=utf-8", []byte(content))
+}
+
+func (accountController AccountController) ServeAdminAccountTransactionPage(c *gin.Context) {
+	content := accountController.getDashboardContent("transactions", c)
+	c.Data(http.StatusOK, "text/html; charset=utf-8", []byte(content))
+}
+
+func (accountController AccountController) ServeAdminAccountBalancePage(c *gin.Context) {
+	content := accountController.getDashboardContent("balance", c)
+	c.Data(http.StatusOK, "text/html; charset=utf-8", []byte(content))
+}
+
+func (accountController AccountController) ServeAdminAccountRecieptPage(c *gin.Context) {
+	content := accountController.getDashboardContent("reciept", c)
+	c.Data(http.StatusOK, "text/html; charset=utf-8", []byte(content))
+}
+
+func (accountController AccountController) ServeAdminAccountSettingsPage(c *gin.Context) {
+	content := accountController.getDashboardContent("settings", c)
+	c.Data(http.StatusOK, "text/html; charset=utf-8", []byte(content))
+}
+
+func (accountController AccountController) getDashboardContent(page string, c *gin.Context) string {
 	paths := []string{
-		"views/html/account/account.html",
+		"views/html/account/header.html",
+		"views/html/account/footer.html",
+		"views/html/account/account_top.html",
+		"views/html/account/account_bottom.html",
+		"views/html/account/overview.html",
+		"views/html/account/transactions.html",
+		"views/html/account/balance.html",
+		"views/html/account/reciept.html",
+		"views/html/account/settings.html",
 	}
 	vars := make(map[string]interface{})
 
@@ -147,15 +213,110 @@ func (accountController AccountController) ServeAccountPage(c *gin.Context) {
 
 	templateEngine := new(services.TemplateEngine)
 
-	accounthtmlPage := templateEngine.ProcessFile(paths[0], vars)
+	header := templateEngine.ProcessFile(paths[0], vars)
+	footer := templateEngine.ProcessFile(paths[1], vars)
+	accounthtmlPageTop := templateEngine.ProcessFile(paths[2], vars)
+	accounthtmlPageBottom := templateEngine.ProcessFile(paths[3], vars)
+	overviewContent := templateEngine.ProcessFile(paths[4], vars)
+	transactionContent := templateEngine.ProcessFile(paths[5], vars)
+	balanceContent := templateEngine.ProcessFile(paths[6], vars)
+	recieptContent := templateEngine.ProcessFile(paths[7], vars)
+	settingsContent := templateEngine.ProcessFile(paths[8], vars)
 
-	c.Data(http.StatusOK, "text/html; charset=utf-8", []byte(accounthtmlPage))
+	switch page {
+	case "transactions":
+
+		return header + accounthtmlPageTop + transactionContent + accounthtmlPageBottom + footer
+
+	case "balance":
+		return header + accounthtmlPageTop + balanceContent + accounthtmlPageBottom + footer
+
+	case "reciept":
+		return header + accounthtmlPageTop + recieptContent + accounthtmlPageBottom + footer
+
+	case "settings":
+		return header + accounthtmlPageTop + settingsContent + accounthtmlPageBottom + footer
+
+	default:
+		return header + accounthtmlPageTop + overviewContent + accounthtmlPageBottom + footer
+
+	}
 
 }
+
+// func (accountController AccountController) ServeAccountPage(c *gin.Context) {
+
+// 	paths := []string{
+// 		"views/html/account/header.html",
+// 		"views/html/account/footer.html",
+// 		"views/html/account/account_top.html",
+// 		"views/html/account/account_bottom.html",
+// 		"views/html/account/overview.html",
+// 		"views/html/account/transactions.html",
+// 		"views/html/account/balance.html",
+// 		"views/html/account/reciept.html",
+// 		"views/html/account/settings.html",
+// 	}
+// 	vars := make(map[string]interface{})
+
+// 	user := accountController.GetAllUserData(c.Query("username"))
+// 	vars["name"] = user.name
+
+// 	templateEngine := new(services.TemplateEngine)
+
+// 	header := templateEngine.ProcessFile(paths[0], vars)
+// 	footer := templateEngine.ProcessFile(paths[1], vars)
+// 	accounthtmlPageTop := templateEngine.ProcessFile(paths[2], vars)
+// 	accounthtmlPageBottom := templateEngine.ProcessFile(paths[3], vars)
+// 	overviewContent := templateEngine.ProcessFile(paths[4], vars)
+// 	transactionContent := templateEngine.ProcessFile(paths[5], vars)
+// 	balanceContent := templateEngine.ProcessFile(paths[6], vars)
+// 	recieptContent := templateEngine.ProcessFile(paths[7], vars)
+// 	settingsContent := templateEngine.ProcessFile(paths[8], vars)
+
+// 	page := "home"
+// 	switch page {
+// 	case "transactions":
+// 		c.Data(http.StatusOK, "text/html; charset=utf-8", []byte(header+accounthtmlPageTop+transactionContent+accounthtmlPageBottom+footer))
+// 		return
+// 	case "balance":
+// 		c.Data(http.StatusOK, "text/html; charset=utf-8", []byte(header+accounthtmlPageTop+balanceContent+accounthtmlPageBottom+footer))
+
+// 		return
+// 	case "reciept":
+// 		c.Data(http.StatusOK, "text/html; charset=utf-8", []byte(header+accounthtmlPageTop+recieptContent+accounthtmlPageBottom+footer))
+
+// 		return
+// 	case "settings":
+// 		c.Data(http.StatusOK, "text/html; charset=utf-8", []byte(header+accounthtmlPageTop+settingsContent+accounthtmlPageBottom+footer))
+
+// 		return
+// 	default:
+// 		c.Data(http.StatusOK, "text/html; charset=utf-8", []byte(header+accounthtmlPageTop+overviewContent+accounthtmlPageBottom+footer))
+// 		return
+
+// 	}
+
+// }
 
 // func status(c *gin.Context) {
 // 	c.JSON(http.StatusOK, gin.H{"status": "You are logged in"})
 // }
+
+func isLoggedIn(c *gin.Context) bool {
+
+	session := sessions.Default(c)
+	user := session.Get(userkey)
+
+	if user == nil {
+
+		return false
+	}
+	usernameSessionstring := fmt.Sprintf("%s", user)
+
+	return usernameSessionstring == "admin"
+
+}
 
 // func me(c *gin.Context) {
 // 	session := sessions.Default(c)
--- routes/router.go
+++ routes/router.go
@@ -9,16 +9,26 @@ func InitRouter(engine *gin.Engine) {
 
 	accountController := new(controllers.AccountController)
 	homeController := new(controllers.HomeController)
-	engine.GET("/404", homeController.Handle404)
+	engine.NoRoute(homeController.Handle404)
 	engine.GET("/", homeController.ServeHomePage)
 	engine.GET("/about", homeController.ServeAboutPage)
 	engine.GET("/login", accountController.HandleLogin)
 	engine.POST("/login", accountController.PerformLogin)
 	// engine.POST("/login", accountController.PerformLogin)
-	engine.POST("/account/:username", accountController.ServeAccountPage)
-	engine.GET("/account/:username", accountController.HandleAccountPage)
+	// engine.POST("/account/:username", accountController.ServeAccountPage)
+	// engine.GET("/account/:username", accountController.HandleAccountPage)
+
+	engine.POST("/account/admin", accountController.ServeAdminAccountPage)
+	engine.GET("/account/admin", accountController.HandleAdminAccountPage)
+	engine.GET("/account/admin/transactions", accountController.ServeAdminAccountTransactionPage)
+	engine.GET("/account/admin/balance", accountController.ServeAdminAccountBalancePage)
+	engine.GET("/account/admin/reciept", accountController.ServeAdminAccountRecieptPage)
+	engine.GET("/account/admin/settings", accountController.ServeAdminAccountSettingsPage)
+	engine.GET("/account/admin/", accountController.ServeAdminAccountOverviewPage)
+	engine.GET("/account/admin/overview", accountController.ServeAdminAccountOverviewPage)
 	engine.GET("/logout", accountController.HandleLogout)
 	engine.GET("/payment", accountController.HandlePayment)
+
 	// engine.GET("/template", accountController.ServeTemplate)
 
 }
