c8ecb795b4ca2e84b95103a6cfd810ed97fe750d
xceejay <joelkofiamoako@gmail.com>
2021-04-03 15:44:37 +0000

    works

--- views/js/main.js
+++ views/js/main.js
@@ -2,9 +2,35 @@ $(document).ready(function () {
 
 
 
-    $('input[type=radio][name=donation_type]').change(function() {
+    $('input[type=radio][name=donation_type]').change(function () {
         $(".unanymous").toggle()
-     
+
 
     })
+
+
+    $('.registerform').validate({
+        rules: {
+            password: {
+                minlength: 5
+            },
+            confirm_password: {
+                minlength: 5,
+                equalTo: "#password"
+            }
+        }
+    });
+
+    $('#registerbtn').click(function (event) {
+        // alert("no")
+        event.preventDefault()
+        $("#passwordnotif").show().delay(2000).fadeOut();
+    });
+
+
+
+
+
+
+
 })
